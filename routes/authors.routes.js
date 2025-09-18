027be3307a71fe046bd83978ba26a20838ad37a5
ebru stagiaire digitalcity <105438469+shebruu@users.noreply.github.com>
2025-09-18 14:20:47 +0200

    add controller

--- routes/authors.routes.js
+++ routes/authors.routes.js
@@ -2,87 +2,18 @@
 
 const { Router } = require('express');
 const router = Router();
-
-//  injection author dans req.author
-// router.use((req, res, next) => {
-//   req.author = req.app.locals.models.author;
-//   next();
-// });
-
-
-function getAuthorModel(req) {
-  return req.app.locals.models.author;
-}
-
-
-router.get('/', async (req, res, next) => {
-  try {
-    const authors = await getAuthorModel(req).findAll();
-    res.json(authors);
-  } catch (err) {
-    next(err);
-  }
-});
+const authorController = require('../controller/author.controller');
 
 
 
-router.get('/:id', async (req, res, next) => {
-  try {
-    const foundAuthor = await getAuthorModel(req).findByPk(req.params.id);
-    if (!foundAuthor) return res.status(404).json({ error: 'Author not found' });
-    res.json(foundAuthor);
-  } catch (err) {
-    next(err);
-  }
-});
+
+router.get('/', authorController.getAllAuthors);
+router.get('/:id', authorController.getAuthorById);
+router.post('/', authorController.createAuthor);
+router.put('/:id', authorController.updateAuthor);
+router.delete('/:id', authorController.deleteAuthor);
 
 
-
-router.post('/', async (req, res, next) => {
-  try {
-    const newAuthor = await getAuthorModel(req).create(req.body);
-    res.status(201).json(newAuthor);
-  } catch (err) {
-    next(err);
-  }
-});
-
-
-router.get('/:id', async (req, res, next) => {
-  try {
-    const foundAuthor = await getAuthorModel(req).findByPk(req.params.id);
-    if (!foundAuthor) return res.status(404).json({ error: 'Author not found' });
-    res.json(foundAuthor);
-  } catch (err) {
-    next(err);
-  }
-});
-
-
-
-router.put('/:id', async (req, res, next) => {
-  try {
-    const author=getAuthorModel(req);
-    const [updated] = await author.update(req.body, { where: { id: req.params.id } });
-    if (!updated) return res.status(404).json({ error: 'Author not found' });
-    const updatedAuthor = await author.findByPk(req.params.id);
-    res.json(updatedAuthor);
-  } catch (err) {
-    next(err);
-  }
-});
-
-
-
-router.delete('/:id', async (req, res, next) => {
-  try {
-    const deleted = await getAuthorModel(req).destroy({ where: { id: req.params.id } });
-    if (!deleted) return res.status(404).json({ error: 'Author not found' });
-    res.json({ message: 'Author deleted' });
-  } catch (err) {
-    next(err);
-  }
-});
 
 
 
